95f735e6cdb1acae518d75f8d9f419a8590ddde0
VictorDMoura <76460793+VictorDMoura@users.noreply.github.com>
2023-04-16 13:19:34 -0300

    Arquivo do projeto

---
+++ script.js
@@ -0,0 +1,78 @@
+const inputBox = document.querySelector(".inputField input")
+const addBtn = document.querySelector(".inputField button")
+const todoList = document.querySelector(".todoList")
+const deleteAllBtn = document.querySelector("footer button")
+
+showTasks()
+
+
+inputBox.onkeyup = () => {
+    let userEnteredValue = inputBox.value
+    if(userEnteredValue.trim() != 0){
+        addBtn.classList.add("active")
+    } else {
+        addBtn.classList.remove("active")
+
+    }
+}
+
+
+addBtn.onclick = () => {
+    let userEnteredValue = inputBox.value
+    let getLocalStorageData = localStorage.getItem("todo_app")
+
+
+    if(getLocalStorageData == null){
+        list = []
+    } else {
+        list = JSON.parse(getLocalStorageData)
+    }
+    list.push(userEnteredValue)
+    localStorage.setItem('todo_app', JSON.stringify(list))
+    showTasks();
+    addBtn.classList.remove("active")
+}
+
+
+function showTasks() {
+    let getLocalStorageData = localStorage.getItem("todo_app")
+    if(getLocalStorageData == null){
+        list = []
+    } else {
+        list = JSON.parse(getLocalStorageData)
+    }
+
+    const pedingTasks = document.querySelector(".pedingTasks")
+    pedingTasks.textContent = list.length
+
+    if(list.length > 0){
+        deleteAllBtn.classList.add("active")
+    } else {
+        deleteAllBtn.classList.remove("active")
+    }
+
+    let newListItems = ""
+    list.forEach((element, index) => {
+        newListItems += `<li>${element} <span class="deleteTask" onclick="deleteTask(${index})">apagar</span></li>`
+    })
+    
+    todoList.innerHTML = newListItems
+    inputBox.value = ""
+}
+
+
+function deleteTask (index) {
+    let getLocalStorageData = localStorage.getItem("todo_app")
+
+    list = JSON.parse(getLocalStorageData)
+    list.splice(index, 1)
+    localStorage.setItem("todo_app", JSON.stringify(list))
+    showTasks()
+}
+
+
+deleteAllBtn.onclick = () => {
+    localStorage.setItem("todo_app", JSON.stringify([]))
+    showTasks()
+
+}
